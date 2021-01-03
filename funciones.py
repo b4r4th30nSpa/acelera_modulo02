73309dd3a3acdd381db7665f06942430d75d8242
b4r4th30nSpa <ivmartinezc@gmail.com>
2021-01-03 18:04:25 +0100

    funciones anónimas

--- funciones.py
+++ funciones.py
@@ -1,3 +1,6 @@
+#----------------------
+#función de orden superior 'returnF' 
+
 def maxi(*l):
     if len(l)==0:
         return 0
@@ -28,7 +31,8 @@ funciones = {'max':maxi,
     'med':media
     }
 
-#esta versión de returnF devuelve la dirección de memoria de una función
+#returnF devuelve la dirección de memoria de una función
+
 def returnF(nombre):
     if nombre in funciones:
         return funciones[nombre]
@@ -38,7 +42,37 @@ def returnF(nombre):
 returnF('min')(1,-4,3,4,9)
 #ejecuta y devuelve -4
 
-#funciones anónimas
+
+#----------------------------
+
+
+def normal(x):
+    return x
+
+
+#sumatoria de la sucesión de 0 a 'hasta'
+#notar que la función recibe unna función 'f' como parámetro (es de orden superior)
+
+def sumaTodos(hasta, f):
+    suma = 0
+    for i in range(0,hasta+1):
+        suma += f(i)
+    return suma
+
+#FUNCIONES ANÓNIMAS pasadas como parámetros (llamadas 'lambdas' en python)
+#paso a sumaTodos una lambda (keyword de Python) como parámetro
+#(en este caso es una función cúbica):
+
+print(sumaTodos(3, lambda x: x**3))
+#notar que tras ':' está, primero, el parámetro (x),
+#si son varios se separan con ',', y luego
+#el bloque que define la función anónima ('x**3')
+
+#esta función anónima me ahorra tener que definir una función 'cubo'
+
+
+    
+    
 
 
 
