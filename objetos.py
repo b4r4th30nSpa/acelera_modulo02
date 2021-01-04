4011f5ddc1ab1284bb4e2fb59e2102e731b94c74
b4r4th30nSpa <ivmartinezc@gmail.com>
2021-01-04 18:41:42 +0100

    Primera clase de objetos

---
+++ objetos.py
@@ -0,0 +1,6 @@
+class Perro: #el nombre de una clase siempre ha de comenzar con mayúsculas
+    def __init__(self,n, e, p): #función constructora
+        self.nombre = n
+        self.edad = e
+        self.peso = p
+    
--- objetos_ejemplo_turtle.py
+++ objetos_ejemplo_turtle.py
@@ -1,10 +1,16 @@
 import turtle
 tortuga=turtle.Turtle()
-
+tortuga.shape('turtle')
 tortuga.fd(50)
+tortuga.speed(5)
 
 otraTortuga=turtle.Turtle()
-
+otraTortuga.color('green')
 otraTortuga.left(90)
 otraTortuga.fd(50)
-otraTortuga.color('green')
+otraTortuga.speed(5)
+
+lentorra=turtle.Turtle()
+lentorra.right(90)
+lentorra.fd(50)
+lentorra.speed(1)
